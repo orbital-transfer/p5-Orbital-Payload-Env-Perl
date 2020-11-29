297d2715eb29cb8831f425020c756d61ec60106d
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-11-29 00:51:35 -0600

    Role application here instead of Orbital::Launch

--- lib/Orbital/Payload/Environment/Perl.pm
+++ lib/Orbital/Payload/Environment/Perl.pm
@@ -1,7 +1,13 @@
+use Orbital::Transfer::Common::Setup;
 package Orbital::Payload::Environment::Perl;
 # ABSTRACT: Tools for Perl development
 
 use strict;
 use warnings;
+
+classmethod apply_roles_to_repo( $repo ) {
+	Moo::Role->apply_roles_to_object( $repo, 'Orbital::Payload::Environment::Perl::Repo::Role::DistZilla');
+	Moo::Role->apply_roles_to_object( $repo, 'Orbital::Payload::Environment::Perl::Repo::Role::CpanfileGit');
+}
 
 1;
