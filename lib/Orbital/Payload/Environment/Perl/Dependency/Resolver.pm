814c33ee7dc5108e962599f3379c5008c258bd66
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-08-08 16:55:39 -0400

    Start of dep resolver

---
+++ lib/Orbital/Payload/Environment/Perl/Dependency/Resolver.pm
@@ -0,0 +1,45 @@
+package Orbital::Payload::Environment::Perl::Dependency::Resolver;
+# ABSTRACT: Resolves Perl dependencies
+
+use Moo;
+use CPAN::FindDependencies;
+use CPAN::Meta;
+use Module::CPANfile;
+
+sub resolve {
+	my ($self, $project_root) = @_;
+
+}
+
+sub root_resolve_cpanmeta {
+	my ($self, $project_root) = @_;
+	for my $meta_file ('META.json', 'META.yml') {
+		my $meta_file_path = $project_root->child( $meta_file );
+		next unless -f $meta_file_path;
+		my $prereqs = CPAN::Meta->load_file( $meta_file_path )->prereqs;
+	}
+}
+
+sub root_resolve_cpanfile {
+	my ($self, $project_root) = @_;
+
+	$self->resolve_cpanfile( )
+}
+
+sub resolve_cpanmeta {
+	my ($self, $meta_file) = @_;
+	CPAN::Meta->load_file( $meta_file );
+	#)->prereqs;
+}
+
+sub resolve_cpanfile {
+	my ($self, $cpanfile) = @_;
+
+}
+
+sub resolve_cpan {
+	my ($self, $cpan_module)= @_;
+	my @dependencies = CPAN::FindDependencies::finddeps($cpan_module);
+}
+
+1;
