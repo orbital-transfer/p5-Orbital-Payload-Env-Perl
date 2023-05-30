cef9ac30078aa974a07d30a26a55dcfe70156537
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-05-29 20:04:25 -0400

    Add on_demand for CLI::Osprey

--- lib/Orbital/CLI/Command/Launch/PodSite.pm
+++ lib/Orbital/CLI/Command/Launch/PodSite.pm
@@ -4,7 +4,7 @@ package Orbital::CLI::Command::Launch::PodSite;
 
 use Orbital::Transfer::Common::Setup;
 use Mu;
-use CLI::Osprey;
+use CLI::Osprey on_demand => 1;
 
 method _setup() {
 	system(qw());
